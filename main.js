5e103bd946147eb9a3dcfaa05b1247e1464e8c4c
Nikita Motytskyi <vt222_mnv@student.ztu.edu.ua>
2023-05-31 10:54:07 +0300

    Added Cards and About sections

---
+++ main.js
@@ -0,0 +1,7 @@
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('header-scroll'); else header.classList.remove('header-scroll')
+}
+window.addEventListener('scroll', scrollHeader)
